b60712016104c68699be00414d7988712da5f807
jsanchez-codes <julian.sanchez@bsu.edu>
2026-01-29 21:53:08 +0000

    Test Code

---
+++ horseRace.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+void advance(int horseNum, int* jorses);
+void printLane(int horseNum, int* horses);
+bool isWinner(int horseNum, int* horses);
+
+const int NUM_HORSES = 5;
+const int TRACK_LENGTH = 15;
+
+int main(){
+	std::cout << "Horse Race Game " << std::endl;
+
+	return 0;
+} // end main
+
+
