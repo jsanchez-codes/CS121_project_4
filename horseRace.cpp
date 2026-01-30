cf57c762c93c9c9e1d8dcb94b9adb4f6efef67ce
jsanchez-codes <julian.sanchez@bsu.edu>
2026-01-30 04:47:49 +0000

    Test Code

--- horseRace.cpp
+++ horseRace.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 #include <ctime>
 
-void advance(int horseNum, int* jorses);
+void advance(int horseNum, int* horses);
 void printLane(int horseNum, int* horses);
 bool isWinner(int horseNum, int* horses);
 
@@ -15,8 +15,29 @@ int main(){
 
 	int horses[NUM_HORSES] = {0, 0, 0, 0, 0};
 
+	// test advance
+	for (int i = 0; i < 10; i++){
+		advance(0, horses);
+		printLane(0, horses);
+	} // end for
 
 	return 0;
 } // end main
 
+void advance(int horseNum, int* horses){
+	int coin = rand() % 2;
+	horses[horseNum] += coin;
+} // end advance
+
+void printLane(int horseNum, int* horses){
+	for (int i = 0; i < TRACK_LENGTH; i++){
+		if (i == horses[horseNum]){
+			std::cout << horseNum;
+		} else {
+			std::cout << ".";
+		} // end if
+	} // end for loop
+	// print a newline
+	std::cout << std::endl;
+} // end printLane
 
